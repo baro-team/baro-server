32ae4eaa69435039468184e023cbeafbd0cec8ac
Yu Jin <qldls0307@naver.com>
2026-04-28 10:12:58 +0900

    feat: add swagger setup to dispatch service

--- dispatch-service/build.gradle.kts
+++ dispatch-service/build.gradle.kts
@@ -6,5 +6,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     runtimeOnly("org.postgresql:postgresql")
 }
