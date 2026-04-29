24b46ee9560bf111d5ddbd06d9c0142935801ff2
Yu Jin <qldls0307@naver.com>
2026-04-29 11:47:21 +0900

    fix: control Jackson 의존성 추가

--- control-service/build.gradle.kts
+++ control-service/build.gradle.kts
@@ -9,5 +9,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-integration")
     implementation("org.springframework.integration:spring-integration-mqtt")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.bouncycastle:bcpkix-jdk18on:1.78.1")
 }
