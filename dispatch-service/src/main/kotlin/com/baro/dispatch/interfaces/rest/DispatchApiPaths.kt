5cdb8059442bd3d6b368304830d18efce7b9225f
Yu Jin <qldls0307@naver.com>
2026-06-15 10:57:37 +0900

    feat: 배차 취소 API 추가 (#70)
    * feat: 배차 취소 API 추가
    
    * fix: 배차 취소 트랜잭션 범위 조정
    
    * docs: Kotlin 안정성 규칙 추가

--- dispatch-service/src/main/kotlin/com/baro/dispatch/interfaces/rest/DispatchApiPaths.kt
+++ dispatch-service/src/main/kotlin/com/baro/dispatch/interfaces/rest/DispatchApiPaths.kt
@@ -5,6 +5,8 @@ object DispatchApiPaths {
     const val PRE_DISPATCH = "/pre"
     const val PRE_DISPATCH_FULL = "$DISPATCH$PRE_DISPATCH"
     const val CONFIRM_DISPATCH_FULL = DISPATCH
+    const val CANCEL_DISPATCH = "/{dispatchId}/cancel"
+    const val CANCEL_DISPATCH_FULL = "$DISPATCH$CANCEL_DISPATCH"
     const val EXPORT_DAILY = "/export/daily"
     const val INTERNAL_DISPATCH = "/internal/dispatch"
     const val EXPORT_DAILY_FULL = "$INTERNAL_DISPATCH$EXPORT_DAILY"
