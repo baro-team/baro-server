51ed5add27f9ebf28139df080d5f8b548a6019f3
ION127 <ion011227@gmail.com>
2026-06-19 15:50:03 +0900

    test: 테스트 yml에 신규 빈 속성 추가로 contextLoads 수정
    - control-service: RelocationServiceClient용 relocation.service.url 추가
    - relocation-service: ControlServiceClient용 control.service.url 추가
    - relocation-service: @SpringBootTest(properties) 오버라이드 제거 (테스트 yml로 충분)

--- relocation-service/src/test/kotlin/com/baro/relocation/SaveWeightServiceApplicationTests.kt
+++ relocation-service/src/test/kotlin/com/baro/relocation/SaveWeightServiceApplicationTests.kt
@@ -3,15 +3,7 @@ package com.baro.relocation
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(
-    properties = [
-        "RELOCATION_DB_URL=jdbc:h2:mem:testdb",
-        "RELOCATION_DB_USERNAME=sa",
-        "RELOCATION_DB_PASSWORD=",
-        "CONTROL_SERVICE_URL=http://localhost:8081",
-        "spring.jpa.hibernate.ddl-auto=none",
-    ],
-)
+@SpringBootTest
 class SaveWeightServiceApplicationTests {
     @Test
     fun `스프링 컨텍스트가 정상적으로 로드된다`() {
